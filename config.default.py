c3598cfad494ba325c510e7e7c44555616080ebb
pharesim <pharesim@protonmail.com>
2024-04-25 15:10:37 +0200

    deleted files

--- config.default.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'hrigahkvnaskgnkbnm8hklmnbldymnkd'
-WIKI_USER = 'wikiuser'
-POSTING_KEY = '5Jnf...'
-ACTIVE_KEY = '5JqE...'
-
-SESSION_TYPE = 'redis'
-SESSION_REDIS = 'redis://127.0.0.1:6379'
-SESSION_USE_SIGNER = True
--- static/js/wiki.js
+++
@@ -1,8 +0,0 @@
-const { colorSyntax, tableMergedCell, uml  } = toastui.Editor.plugin;
-const viewer = toastui.Editor.factory({
-    el: document.getElementById('editor'),
-    initialValue: document.getElementById('editor').innerHTML,
-    viewer: true,
-    plugins: [latexPlugin, tableMergedCell, uml],
-    extendedAutolinks: true
-});
