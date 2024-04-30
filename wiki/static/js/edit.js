ee32573132629e0c60918f8121a9bc53e90432d3
pharesim <pharesim@protonmail.com>
2024-04-30 05:11:02 +0200

    added to alert message after successful edit

--- wiki/static/js/edit.js
+++ wiki/static/js/edit.js
@@ -116,7 +116,7 @@ function broadcastEdit(title,body,permlink,t,reason) {
         'Posting',
         (response) => {
             if(response.success == true) {
-                alert("Edit successful, waiting for blockchain sync. If your changes aren't visible right away, try reloading the page.");
+                alert("Edit successful, waiting for blockchain sync. If your changes aren't visible right away, try reloading the page after a few seconds.");
                 setTimeout(() => { window.location.replace("/wiki/"+permlink) }, 2000);
             } else {
                 btn.style.display = 'block';
