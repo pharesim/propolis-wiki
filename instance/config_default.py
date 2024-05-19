6a9d56335bf43501be497a57326f7e6221e2916b
pharesim <pharesim@protonmail.com>
2024-05-19 03:22:01 +0200

    webhook

--- instance/config_default.py
+++ instance/config_default.py
@@ -8,6 +8,6 @@ DATABASE = 'wiki' # postgresql database
 DB_USERNAME = 'wikiuser'
 DB_PASSWORD = 'wikipassword'
 
-DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/1234567890/abcdefg'
+DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/1234567890/abcdefg' # change to your webhook if wanted
 WAVES_ACCOUNT = '' # set to activate posting to ecency.waves
 LEOTHREADS_ACCOUNT = '' # set to activate posting to leothreads
