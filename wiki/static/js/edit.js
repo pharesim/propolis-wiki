c4ea5720e2f66d4d4e9f4b76a2d4192451c954c4
HiveTrending <hive_up@protonmail.com>
2024-06-06 20:05:42 -0700

    Fix bug where internal links with fragments lose the fragment from the linked text

--- wiki/static/js/edit.js
+++ wiki/static/js/edit.js
@@ -196,13 +196,11 @@ btn.addEventListener('click', function() {
     const replaceInternalLinks = (body) => {
         body = body.replaceAll('](/wiki/','](/@'+wiki_user+'/').replaceAll('<a href="/wiki/','<a href="/@'+wiki_user+'/').replaceAll('<ref>|Reference: ','<ref>').replaceAll('<ref>','<ref>|Reference: ');
         body = body.replaceAll(/\[\[([^\]]+)\]\]/g, (match, p1, offset, string, groups) => {
-            let link = p1;
-            const hasFragment = link.includes('|')
-            const linkFragment = hasFragment ? '#' + link.split('|')[1].replaceAll(' ','') : ''
-            if (hasFragment) {
-                link = link.split('|')[0]
-            }
-            return `[${link}](/@${wiki_user}/${link.split(' ').map(capitalizeFirstLetter).join('-')}${linkFragment})`
+            let link = p1.trim();
+            const hasFragment = link.includes('|');
+            const linkFragment = hasFragment ? '#' + link.split('|')[1].replaceAll(' ','') : '';
+            const linkWithoutFragment = hasFragment ? link.split('|')[0] : link;
+            return `[${link}](/@${wiki_user}/${linkWithoutFragment.split(' ').map(capitalizeFirstLetter).join('-')}${linkFragment})`
         })
 
         return body
